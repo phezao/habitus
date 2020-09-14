f1b15e4735a0407c0a6cb66f5897be0e5efac20c
Philip Oka <philip.oka@gmail.com>
2020-09-14 09:53:17 +0100

    created the habit model and create a little test

---
+++ db/migrate/20200914084436_create_habits.rb
@@ -0,0 +1,11 @@
+class CreateHabits < ActiveRecord::Migration[5.2]
+  def change
+    create_table :habits do |t|
+      t.string :name
+      t.string :state
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "User has many habits" do
+    user = User.create(email: "joao@mail.com", password: "123456", name: "João")
+    habit = Habit.create(name: "Exercise", state: "not done yet", user: user)
+    assert_equal 1, user.habits.size
+  end
 end
