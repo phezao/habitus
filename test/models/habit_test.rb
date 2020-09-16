9d7640c61e620cf3959fd6c0481cebce9a328b6e
Philip Oka <philip.oka@gmail.com>
2020-09-16 18:50:12 +0100

    created some tests for the validation and associations

--- test/models/habit_test.rb
+++ test/models/habit_test.rb
@@ -1,10 +1,23 @@
 require 'test_helper'
 
 class HabitTest < ActiveSupport::TestCase
-  test "User must exist in a Habit" do
-    user = User.create(name: "Maria", email: "maria@gmail.com", password: "123456")
-    habit = Habit.create(name: "Pular corda", user: user)
+  def setup
+    @user = User.create(name: "Maria", email: "maria@gmail.com", password: "123456")
+    @habit = Habit.create(name: "Pular corda", user: @user)  
+  end
+  test "Testing the relationship between the user and the habit, a habit has a user" do
     
-    assert_not_nil habit.user 
+    assert_not_nil @habit.user 
+  end
+
+  test "Shouldn't save habit in the db if there's no name" do
+    @habit.update(name: nil)
+
+    assert_not @habit.save
+  end
+  test "Shouldn't save habit in the db if there's no user" do
+    @habit.update(user: nil)
+  
+    assert_not @habit.save
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "User has many habits" do
+  test "Testing the relationship of habits with user, user has many habits" do
     user = User.create(email: "joao@mail.com", password: "123456", name: "João")
     habit = Habit.create(name: "Exercise", state: "not done yet", user: user)
     habit = Habit.create(name: "Code", state: "not done yet", user: user)
